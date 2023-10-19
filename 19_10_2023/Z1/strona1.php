dc5724bbf76faa650c7f8bada186948ddae60aa1
MaksymilianZarzycki <jeden.maks@gmail.com>
2023-10-19 14:51:01 +0200

    Zad1

---
+++ 19_10_2023/Z1/strona1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <title>Logowanie</title>
+</head>
+<body>
+        <form method="post" action="strona2.php">
+            <label for="username">Nazwa użytkownika: </label>
+            <input type="text" name="username">
+            <label for="password">Hasło: </label>
+            <input type="password" name="password">
+            <input type="submit" value="Zaloguj">
+        </form>
+</body>
+</html>
---
+++ 19_10_2023/Z1/strona2.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+         $username = $_POST['username'];
+         $password = $_POST['password'];
+
+         $host="localhost";
+         $user="root";
+         $pass="";
+         $db="5cmz";
+
+         $connection= mysqli_connect($host,$user,$pass,$db);
+
+         $statement = "SELECT * FROM `dane_logowania` WHERE `Username`='$username'";
+    
+         $result = mysqli_query($connection,$statement);
+         
+         $row=mysqli_fetch_row($result);
+        
+        if($row==NULL)
+        {
+            echo "Dane logowania niepoprawne";
+        }else if($row[2]==$password)
+         {
+            echo "Zalogowano";
+         } else echo "Dane logowania niepoprawne";
+    ?>
+</body>
+</html>
