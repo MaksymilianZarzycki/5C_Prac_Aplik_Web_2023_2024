93931a17d2edfe73032d219ef2a93515deec4c3c
MaksymilianZarzycki <jeden.maks@gmail.com>
2023-10-26 16:06:52 +0200

    baza i strona

---
+++ 26_10_2023/airport.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <title>Odloty samolotów</title>
+    <linjk rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header id="hleft">
+        <h2>Odloty z lotniska</h2>
+    </header>
+
+    <header id="hright">
+        <img src="zad6.png" alt="logotyp">
+    </header>
+
+    <main>
+        <h4>Tabela odlotów</h4>
+        <table>
+            <tr>
+                <th>ID</th>
+                <th>NUMER REJSU</th>
+                <th>CZAS</th>
+                <th>KIERUNEK</th>
+                <th>STATUS</th>
+            </tr>
+        </table>
+    </main>
+
+    <footer id="fleft">
+        <a href="kw1.jpg" target="_blank">Pobierz obraz</a>
+    </footer>
+
+    <footer id="fmiddle">
+        
+    </footer>
+
+    <footer id="fright">
+        Autor: 32
+    </footer>
+
+</body>
+</html>
